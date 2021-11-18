11cb003001f77b61b3ae1cbd341abce9387722e4
VaradMash <VaradMash@users.noreply.github.com>
2021-11-18 21:20:55 +0530

    Added code for checking user login and selective activity launch.

--- app/src/main/java/com/example/smartcityportal/MainActivity.java
+++ app/src/main/java/com/example/smartcityportal/MainActivity.java
@@ -6,22 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     TinyDB tinyDB;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     public void onGetStartedPressed(View view) {
-        try {
-            tinyDB = new TinyDB(getApplicationContext());
-            if (tinyDB.getBoolean("userIsLoggedIn")) {
-                Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
-                startActivity(intent);
-            }
-            else {
-                Intent intent = new Intent(getApplicationContext(), RegisterScreenActivity.class);
-                startActivity(intent);
-            }
-        } catch (Exception e) {}
+//        try {
+//            tinyDB = new TinyDB(getApplicationContext());
+//            if (tinyDB.getBoolean("userIsLoggedIn")) {
+//                Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
+//                startActivity(intent);
+//            }
+//            else {
+//                Intent intent = new Intent(getApplicationContext(), RegisterScreenActivity.class);
+//                startActivity(intent);
+//            }
+//        } catch (Exception e) {
+        Intent intent;
+        if (user != null)
+        {
+            intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
+        }
+        else
+        {
+            intent = new Intent(getApplicationContext(), RegisterScreenActivity.class);
+        }
+        startActivity(intent);
     }
 
     @Override
@@ -31,13 +46,19 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        try {
-            tinyDB = new TinyDB(getApplicationContext());
-            if (tinyDB.getBoolean("userIsLoggedIn")) {
-                Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
-                startActivity(intent);
-            }
-        } catch (Exception e) {}
-
+//        try {
+//            tinyDB = new TinyDB(getApplicationContext());
+//            if (tinyDB.getBoolean("userIsLoggedIn")) {
+//                Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
+//                startActivity(intent);
+//            }
+//        } catch (Exception e) {}
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        if (user != null)
+        {
+            Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
+            startActivity(intent);
+        }
     }
 }
