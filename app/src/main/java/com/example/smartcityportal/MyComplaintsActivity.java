3ffde633a43214c4b305efed732b22700f229aa4
VaradMash <VaradMash@users.noreply.github.com>
2021-11-18 22:57:37 +0530

    Removed redundant back press overrides and added code to capture value for complaints count.

--- app/src/main/java/com/example/smartcityportal/MyComplaintsActivity.java
+++ app/src/main/java/com/example/smartcityportal/MyComplaintsActivity.java
@@ -18,17 +18,4 @@ public class MyComplaintsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_complaints);
         getSupportActionBar().hide();
     }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        /*
-         *  Input   :   Back button press
-         *  Utility :   Route to RegisterComplaintActivity.
-         *  Output  :   Activity launch
-         */
-        Intent intent = new Intent(getApplicationContext(), RegisterComplaintActivity.class);
-        startActivity(intent);
-        MyComplaintsActivity.this.finish();
-    }
 }
--- app/src/main/java/com/example/smartcityportal/MyProfileActivity.java
+++ app/src/main/java/com/example/smartcityportal/MyProfileActivity.java
@@ -1,16 +1,27 @@
 package com.example.smartcityportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -18,6 +29,7 @@ public class MyProfileActivity extends AppCompatActivity {
         this.finish();
     }
 
+    private DocumentReference user_document;
     TextInputEditText usernameTextInputEditText;
     TextInputEditText emailTextInputEditText;
     TextInputEditText phoneTextInputEditText;
@@ -25,6 +37,7 @@ public class MyProfileActivity extends AppCompatActivity {
     String username;
     String email;
     String phone;
+    String complaint_count;
     TinyDB tinyDB;
 
     public void setValues() {
@@ -32,9 +45,31 @@ public class MyProfileActivity extends AppCompatActivity {
             username = tinyDB.getString("userName");
             email = tinyDB.getString("userEmail");
             phone = tinyDB.getString("userPhoneNumber");
+            // capture value for number of complaints
+            phone = "+91" + phone;
+            user_document = FirebaseFirestore.getInstance().collection("user_data").document(phone);
+            user_document.get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            DocumentSnapshot user_document = task.getResult();
+                            if (task.isSuccessful())
+                            {
+                                complaint_count = user_document.get("total_complaints").toString();
+                                complaintCountTextView.setText(complaint_count);
+                            }
+                            else
+                            {
+                                Toast.makeText(MyProfileActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                complaintCountTextView.setText("No data");
+                            }
+                        }
+                    });
             usernameTextInputEditText.setText(username);
             emailTextInputEditText.setText(email);
             phoneTextInputEditText.setText(phone);
+
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
@@ -58,7 +93,6 @@ public class MyProfileActivity extends AppCompatActivity {
         emailTextInputEditText = findViewById(R.id.email2TextInputEditText);
         phoneTextInputEditText = findViewById(R.id.phone2TextInputEditText);
         complaintCountTextView = findViewById(R.id.complaintCountTextView);
-
         tinyDB = new TinyDB(getApplicationContext());
 
         setValues();
