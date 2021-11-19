f2a9b9dc89bf3923e7fa8f807f06e36ad9710671
VaradMash <VaradMash@users.noreply.github.com>
2021-11-19 15:57:49 +0530

    Fixed log out bug and updated database configuration for device verification.

--- app/src/main/java/com/example/smartcityportal/RegisterComplaintActivity.java
+++ app/src/main/java/com/example/smartcityportal/RegisterComplaintActivity.java
@@ -200,6 +200,7 @@ public class RegisterComplaintActivity extends AppCompatActivity {
                 mAuth.signOut();
                 intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+                RegisterComplaintActivity.this.finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
